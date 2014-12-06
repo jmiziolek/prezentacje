c5008896020de8feed1ef589b720337ebd930a78
Jakub Miziołek <jakub.miziolek@contractors.roche.com>
2014-12-06 13:11:42 +0100

    Wordupagain

---
+++ plugin/speech-control/speech-control.js
@@ -0,0 +1,29 @@
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+//recognition.interimResults = true;
+recognition.lang = 'pl';
+recognition.onerror = function(event){console.error(event);};
+recognition.onresult = function(event){
+    str = event.results[event.resultIndex][0].transcript;
+    switch(true){
+        case /dalej|dawaj|wio/.test(str):
+            if(Reveal){
+                Reveal.next();
+            }
+            if(window.opener && window.opener.Reveal){
+                window.opener.Reveal.next();
+            }
+        break;
+        case /cofaj|cofnij|wstecz/.test(str):
+            if(Reveal){
+                Reveal.prev();
+            }
+            if(window.opener && window.opener.Reveal){
+                window.opener.Reveal.prev();
+            }
+        break;
+        default:
+            console.warn(str);
+    }
+};
+//recognition.start();
